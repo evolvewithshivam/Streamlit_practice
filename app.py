a0ab1c9d427cc4f687d7b194244150f78a782233
evolvewithshivam <evolvewithshivam@gmail.com>
2024-11-07 14:36:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ st.color_picker("Pick a color")
 # Use widgets' returned values in variables:
 for i in range(int(st.number_input("Num:"))):
     foo()
-if st.sidebar.selectbox("I:",["f"]) == "f":
-    b()
+#if st.sidebar.selectbox("I:",["f"]) == "f":
+ #   b()
 my_slider_val = st.slider("Quinn Mallory", 1, 88)
 st.write(slider_val)
 
